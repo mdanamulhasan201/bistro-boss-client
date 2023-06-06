e340f3602242ad3f96b16d1a1396cd9504ec21a9
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2023-06-06 09:08:04 +0600

    added

--- src/pages/DeshBoard/Mycart/AddItem/AddItem.jsx
+++ src/pages/DeshBoard/Mycart/AddItem/AddItem.jsx
@@ -1,37 +1,75 @@
 import SectionTitle from "../../../../components/sectionTitle/SectionTitle";
 
 
+
 const AddItem = () => {
     return (
-        <div className="w-full">
+        <div className="w-full mx-auto px-10 bg-slate-100">
             <SectionTitle
                 subHeading="What's New"
                 heading="Add an Item"
             ></SectionTitle>
 
-            <form>
+            <form className="p-5 md:w-[992px] mx-auto">
 
                 <div className="form-control w-full max-w-xs">
                     <label className="label">
-                        <span className="label-text font-semibold">Recipe Name*</span>
+                        <span className="label-text font-bold">Recipe Name*</span>
                     </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Recipe Name" className="input input-bordered w-full max-w-xs" />
                 </div>
+
+
+                <div className="flex gap-5">
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text font-bold">Category*</span>
+
+                        </label>
+                        <select className="select select-bordered">
+                            <option disabled selected>Category</option>
+                            <option>Pizza</option>
+                            <option>Soup</option>
+                            <option>Salad</option>
+                            <option>Dessert</option>
+                            <option>Drinks</option>
+
+                        </select>
+
+                    </div>
+
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text font-bold">Price*</span>
+                        </label>
+                        <input type="number" placeholder="price" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                </div>
+
+
+
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text font-bold">Recipe Details*</span>
+
+                    </label>
+                    <textarea className="textarea textarea-bordered h-72 md:w-[892px]" placeholder="Recipe Details"></textarea>
+
+                </div>
+
                 <div className="form-control w-full max-w-xs">
                     <label className="label">
-                        <span className="label-text">Pick the best fantasy franchise</span>
-                        <span className="label-text-alt">Alt label</span>
+                        <span className="label-text font-bold">Item Image*</span>
+
                     </label>
-                    <select className="select select-bordered">
-                        <option disabled selected>Pick one</option>
-                        <option>Star Wars</option>
-                        <option>Harry Potter</option>
-                        <option>Lord of the Rings</option>
-                        <option>Planet of the Apes</option>
-                        <option>Star Trek</option>
-                    </select>
-                   
+                    <input type="file" className="file-input file-input-bordered w-full max-w-xs" />
+
                 </div>
+
+                <input className="btn btn-outline mt-4 text-[#BB8506] bg-slate-100 hover:border-[#BB8506] hover:text-[#BB8506] border-0 border-b-4" type="submit" value="Add Item" />
+
             </form>
         </div>
     );
